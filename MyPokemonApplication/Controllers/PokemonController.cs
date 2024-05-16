0f0c5cd4ba454ec2b1f294e3bff3f7354ea78a60
Francisco_KevinNico_BSIT32E2 <161100540+Francisco-KevinNico-BSTI32E2@users.noreply.github.com>
2024-05-16 17:20:02 +0800

    Done Editing

--- MyPokemonApplication/Controllers/PokemonController.cs
+++ MyPokemonApplication/Controllers/PokemonController.cs
@@ -19,18 +19,18 @@ namespace MyPokemonApplication.Controllers
 
         public async Task<IActionResult> Index(int page = 1)
         {
-            int pageSize = 20; // number of items per page
+            int pageSize = 20; 
 
-            // Fetch data from the PokéAPI
+            
             var response = await _client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon?offset={(page - 1) * pageSize}&limit={pageSize}");
             var data = JsonConvert.DeserializeObject<dynamic>(response);
 
-            // Get the total count of Pokemon
+            
             int totalPokemon = data.count;
 
             int totalPages = (int)Math.Ceiling((double)totalPokemon / pageSize);
 
-            // Convert the data to a list of Pokemon
+           
             var pokemon = ((IEnumerable<dynamic>)data.results).Select(x => new Pokemon { Name = x.name.ToString() }).ToList();
 
             return View(pokemon);
@@ -40,17 +40,17 @@ namespace MyPokemonApplication.Controllers
 
         public async Task<IActionResult> Details(string name)
         {
-            // Fetch data from the PokéAPI
+            
             var response = await _client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{name}");
             var data = JsonConvert.DeserializeObject<dynamic>(response);
 
-            Console.WriteLine($"API Response: {response}"); // Log the API response
+            Console.WriteLine($"API Response: {response}"); 
 
-            // Convert JArrays to List<Move> and List<Ability>
+           
             var moves = data.moves != null ? ((Newtonsoft.Json.Linq.JArray)data.moves).ToObject<List<Move>>() : new List<Move>();
             var abilities = data.abilities != null ? ((Newtonsoft.Json.Linq.JArray)data.abilities).ToObject<List<Ability>>() : new List<Ability>();
 
-            // Create a Pokemon object
+           
             var pokemon = new Pokemon
             {
                 Name = data.name,
@@ -58,7 +58,7 @@ namespace MyPokemonApplication.Controllers
                 Abilities = abilities
             };
 
-            Console.WriteLine($"Pokemon Object: {JsonConvert.SerializeObject(pokemon)}"); // Log the Pokemon object
+            Console.WriteLine($"Pokemon Object: {JsonConvert.SerializeObject(pokemon)}");
 
             return View(pokemon);
         }
