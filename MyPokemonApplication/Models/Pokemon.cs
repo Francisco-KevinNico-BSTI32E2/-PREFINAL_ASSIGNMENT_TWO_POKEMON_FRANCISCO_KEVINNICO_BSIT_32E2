61382582f71312b2d22db42cba8f05ce95459ce9
Francisco_KevinNico_BSIT32E2 <161100540+Francisco-KevinNico-BSTI32E2@users.noreply.github.com>
2024-05-16 15:05:34 +0800

    Pokemon.cs

---
+++ MyPokemonApplication/Models/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPokemonApplication.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<string> Moves { get; set; }
+        public List<string> Abilities { get; set; }
+    }
+}
