729afddd6dcc239f0f3aefab12caac742a8f8cba
Francisco_KevinNico_BSIT32E2 <161100540+Francisco-KevinNico-BSTI32E2@users.noreply.github.com>
2024-05-16 17:38:24 +0800

    .

--- MyPokemonApplication/Models/Move.cs
+++ MyPokemonApplication/Models/Move.cs
@@ -9,33 +9,12 @@ namespace MyPokemonApplication.Models
 
         public override string ToString()
         {
-            var moveDetails = new List<string>();
-
             if (move != null && !string.IsNullOrEmpty(move.name))
             {
-                moveDetails.Add($"Move Name: {move.name}");
+                return $"Move Name: {move.name}";
             }
 
-            if (version_group_details != null && version_group_details.Any())
-            {
-                var versionGroupDetails = string.Join(", ", version_group_details.Select(v => v.ToString()));
-                moveDetails.Add($"Version Group Details: {versionGroupDetails}");
-            }
-
-            return string.Join(", ", moveDetails);
-        }
-    }
-
-    public class VersionGroupDetail
-    {
-        public int level_learned_at { get; set; }
-        public MoveLearnMethod move_learn_method { get; set; }
-        public VersionGroup version_group { get; set; }
-
-        public override string ToString()
-        {
-            return $"Level Learned At: {level_learned_at}, Move Learn Method: {move_learn_method?.name}, Version Group: {version_group?.name}";
+            return string.Empty;
         }
     }
 }
-
